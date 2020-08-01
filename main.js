cd05d022982dce395190d2106b7225b6b33c718d
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2020-08-01 07:43:03 +0200

    js functionality added

--- main.js
+++ main.js
@@ -3,16 +3,23 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 //Update count and total
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
-  total.innerText = selectedSeatsCount * ticketPrice
+  total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Move select event 
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+// Seat clisk event
 container.addEventListener('click', (e) => {
   if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
     e.target.classList.toggle('selected');
